8b79cdeda7117e0102f015a211aff9dc779c4e1e
gonzaloescobar <gonzalo.david.escobar@gmail.com>
2018-01-22 21:57:55 -0300

    Counter return all lower case when input is HEllO

--- test/models/counter_spec.rb
+++ test/models/counter_spec.rb
@@ -23,43 +23,5 @@ describe 'counter' do
     expect(map["l"]).to eq 2
     expect(map["o"]).to eq 1
   end
-
-  it '"should ignore ! symbol when input hi!"' do
-    @counter = Counter.new
-    
-    map = @counter.countCharacters("hi!")
-    expect(map["h"]).to eq 1
-    expect(map["i"]).to eq 1
-    expect(map["!"]).to eq nil
-
-  end
-
-    it '"should ignore ? symbol when input hi?"' do
-    @counter = Counter.new
-    
-    map = @counter.countCharacters("hi?")
-    expect(map["h"]).to eq 1
-    expect(map["i"]).to eq 1
-    expect(map["?"]).to eq nil
-
-  end
-
-  it '"should omit accent marks so a = á when input papá"' do
-    @counter = Counter.new
-    
-    map = @counter.countCharacters("papá")
-    expect(map["p"]).to eq 2
-    expect(map["a"]).to eq 2
-    
-  end
-
-  it '"should omit accent marks so a = ä when input papá"' do
-    @counter = Counter.new
-    
-    map = @counter.countCharacters("päpa")
-    expect(map["p"]).to eq 2
-    expect(map["a"]).to eq 2
-    
-  end
-
+  
 end
